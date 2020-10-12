72ebe860c48e93178bf9be4534674af1815aa36b
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-10-12 11:42:07 +0600

    not working background location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationEvents, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation/ngx';
 
+declare var window;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -23,33 +24,40 @@ export class AppComponent {
     this.initializeApp();
 
 
-    let config: BackgroundGeolocationConfig = {
-      desiredAccuracy: 10,
-      stationaryRadius: 20,
-      distanceFilter: 30,
-      debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-      stopOnTerminate: false, // enable this to clear background location settings when the app terminates
-    };
-
-    this.backgroundGeolocation.configure(config)
-      .then(() => {
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
-          console.log('bbackgroud location find');
-          console.log(location);
-
-          // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-          // and the background-task may be completed.  You must do this regardless if your operations are successful or not.
-          // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-          this.backgroundGeolocation.finish(); // FOR IOS ONLY
-        });
-      });
-
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+
+
+      let config: BackgroundGeolocationConfig = {
+        desiredAccuracy: 10,
+        stationaryRadius: 20,
+        distanceFilter: 30,
+        debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+        stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+      };
+
+      this.backgroundGeolocation.configure(config)
+        .then(() => {
+          this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
+            console.log('bbackgroud location find');
+            console.log(location);
+
+            var locationstr = localStorage.getItem("location");
+            if (locationstr == null) {
+              this.arr.push(location);
+            } else {
+              var locationarr = JSON.parse(locationstr)
+              this.arr = locationstr
+            }
+            localStorage.setItem("location", JSON.stringify(this.arr));
+          })
+        })
+      window.app = this;
     });
   }
 }
